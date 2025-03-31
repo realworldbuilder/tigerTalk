4b5d075b8c5d5968c268dbfe5bf2959993f803ae
realworldbuilder <foundervisions@proton.me>
2025-03-30 21:54:51 -0400

    Add auth pages and fix middleware for Clerk

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-12">
+      <SignUp path="/sign-up" routing="path" />
+    </div>
+  );
+} 
--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,13 @@ import { authMiddleware } from '@clerk/nextjs';
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/'],
+  publicRoutes: [
+    '/',
+    '/sign-in(.*)',
+    '/sign-up(.*)',
+  ],
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
